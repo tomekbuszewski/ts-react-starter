171fbc334834c2b6df3991ae0af1bd5205d33681
Tomek Buszewski <tomek.buszewski@gmail.com>
2020-08-13 20:22:44 +0200

    chore: storybook updated to v6

--- @config/storybook/webpack.config.js
+++ @config/storybook/webpack.config.js
@@ -1,13 +1,9 @@
-const { resolve, rules } = require("../webpack/common");
+const { resolve } = require("../webpack/common");
 
 module.exports = ({ config }) => ({
   ...config,
   resolve: {
     ...config.resolve,
     ...resolve,
-  },
-  module: {
-    ...config.module,
-    rules: [...config.module.rules, ...rules],
   },
 });
