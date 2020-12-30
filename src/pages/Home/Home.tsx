5866a20b5094314be304d0b3af2a805fbc9d39d7
Tomek Buszewski <tomek.buszewski@gmail.com>
2020-12-30 23:36:30 +0100

    refactor: new webpack

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const Home: React.FunctionComponent = () => (
-  <React.Fragment>Hello from home page</React.Fragment>
+  <React.Fragment>Hello from home</React.Fragment>
 );
 
 export { Home };
